1cacb45dce8e72f40a85b0150ac10877dfc6a126
KavetiRohith <rohitkaveti@gmail.com>
2023-08-27 22:22:48 -0700

    add uptycs image

--- src/portfolio.js
+++ src/portfolio.js
@@ -191,6 +191,7 @@ const workExperiences = {
     {
       role: "Software Engineer",
       company: "Uptycs",
+      companylogo: require("./assets/images/Uptycs.jpg"),
       date: "May 2021 – Jan 2022",
       desc: "Worked on the Osquery Team at Uptycs",
       descBullets: [
